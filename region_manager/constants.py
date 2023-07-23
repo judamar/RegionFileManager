a9516842e1e66d53dc7ed2b63b359ed4ce9b57d4
judamar <juancho.o.mart@gmail.com>
2023-07-23 16:30:35 -0500

    removing constants and funcions files

--- region_manager/constants.py
+++
@@ -1,6 +0,0 @@
-from mcdreforged.api.utils.serializer import Serializable
-
-class Configure(Serializable):
-    command: str = '!!rfm'
-    source_path: str = './server/world/regions/'
-    permission: int = 0
