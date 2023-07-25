d076b7c932c3616f9c267fe25e5087eed52cf1e3
judamar <juancho.o.mart@gmail.com>
2023-07-24 23:14:02 -0500

    adding new options

--- region_manager/config.py
+++ region_manager/config.py
@@ -2,8 +2,16 @@ from mcdreforged.api.utils.serializer import Serializable
 
 class Configure(Serializable):
     prefix: str = '!!rfm'
-    recognized_cmds : tuple = 'export', 'remove'
+    recognized_cmds : list =  [
+        "save",
+        "restore",
+        "remove",
+        "list",
+        "del",
+        "abort"
+    ]
     regions_path_over: str = './server/world/region/'
     regions_path_nether: str = './server/world/DIM-1/region/'
     regions_path_end: str = './server/world/DIM1/region/'
-    permission: int = 3
+    do_backup_before_something: bool = False
+    minimum_permission_level: int = 3
