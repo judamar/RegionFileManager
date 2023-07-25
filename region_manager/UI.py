8422e4aecff94cd76bcf31a545a510df671949cc
judamar <juancho.o.mart@gmail.com>
2023-07-24 23:13:36 -0500

    adding new options

--- region_manager/UI.py
+++ region_manager/UI.py
@@ -1,20 +1,25 @@
-from mcdreforged.api.rtext import *
+from mcdreforged.api.rtext import RTextList, RText, RColor
 from region_manager.config import Configure
 
 conf = Configure()
 prefix = conf.prefix
 
-help_head = """================== §b Region File Manager §r=================="""
+help_head = """================== §b Region File Manager §r==================
+"""
 help_body = {
     f"§b{prefix}": "§rDisplay help message.",
-    f"§b{prefix} export <x> <z> <dimension>": "§rShow the list of region files.",
-    f"§b{prefix} remove <x> <z> <dimension>": "§rRemove the specified region file.",
+    f"§b{prefix} save <x> <z> <dimension> <name>": "§rSave the region in a new folder.",
+    f"§b{prefix} restore <name>": "§rRestore the specified region.",
+    f"§b{prefix} remove <x> <z> <dimension>": "§rRemove the specified region file from world.",
+    f"§b{prefix} list": "§rShow a list with the regions saved.",
+    f"§b{prefix} del": "§rDelete a region from list."
 }
 
 def gen_help_message():
     help_message = RTextList(
         RText(help_head),
-        RText("Available Commands:"))
+        RText("Available Commands:", color=RColor.gold),
+    )
     for command, description in help_body.items():
         help_message.append(f"\n  {command} - {description}")
     return help_message
